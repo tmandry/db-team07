f00a5cacb6fcd35db105cdf3217f5a763f45eede
Joshua Dover <gerfuls@gmail.com>
2013-02-04 14:41:06 -0600

    Documented database and record classes with Doxygen

--- database.h
+++ database.h
@@ -1,19 +1,56 @@
 #ifndef _DATABASE_H_
 #define _DATABASE_H_
 
+#include <string>
+#include <vector>
+#include <map>
+
 #include "record.h"
 #include "table.h"
 
+/** Database is the entry point for creating tables, deleting records,
+    and running queries.
+ */
 class Database {
 public:
-  Database();
+  Database(); //!< Creates an empty database
 
-  void create_table(string name, Table table);
+  //! Add a table to the database
+  /**
+    \param name what to call the table in the database
+    \param table the Table to be inserted into the table
+    \sa Table
+   */
+  void create_table(string name, Table* table);
+
+  //! Remove a table from the database
+  /**
+    \param name which table to remove from the database
+    \sa list_tables()
+   */
   void drop_table(string name);
-  vector<string> list_tables();
-  vector<Table> get_tables();
 
-  Table query(string select, string from, string where);
+  //! Get a list of all the tables currently in the database
+  vector<string> list_tables();
+
+  //! Get pointers to all of the tables currently in the database
+  map<string, Table*> get_tables();
+
+  //! Perform a query on the database
+  /**
+    \param select which columns to include in the returned Table
+    \param from which table to query from
+    \param where the conditions for the query to match
+    \return A pointer to Table with all of the records that match the query
+   */
+  Table* query(string select, string from, string where);
+
+  //! Delete all records that match the query
+  /**
+    \param select which columns to include in the returned Table
+    \param from which table to query from
+    \param where the conditions for the query to match
+   */
   void delete(string select, string from, string where);
 };
 
--- record.h
+++ record.h
@@ -1,14 +1,41 @@
-#ifndef _RECORD_H
-#define _RECORD_H
+#ifndef _RECORD_H_
+#define _RECORD_H_
 
-#include <vector>
-#include <string>
+#include <map>
+#include<string>
+using namespace std;
 
+/**
+ * The record class allows for read and write access of attribute values.
+ */
 class Record {
+
 public:
-	vector<string> stringVector; // this could be a different container
-	void access_entry(string entry);
-private:
+  Record();
+  //! Create a record with existing entries
+  /**
+    \param new_entries a std::map where the key is the name of the entry and the
+           value is the value of the attribute.
+   */
+  Record(map<string, string> new_entries); //!< Create a record with present entries
+
+  ~Record();
+
+  template <typename T>;
+  //! Get the value of an attribute
+  /**
+    \param T the expected type of the attribute
+    \param attribute the name of the attribute (column) in the record.
+   */
+  T get(string attribute);
+
+  template <typename S>;
+  //! Set the value of an attribute
+  /**
+    \param T the expected type of the attribute
+    \param attribute the name of the attribute (column) in the record.
+   */
+  bool set(string attribute, <S> value); //!< Set the value of an attribute, returns true unless there was an error
 
 };
 
